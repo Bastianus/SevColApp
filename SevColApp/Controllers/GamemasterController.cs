560a1122abbba73fab86a81c9a49d5441b8c6a2d
Bas Mattern <notgoingtohappen@live.nl>
2021-03-05 12:06:37 +0100

    game master pagina index start

---
+++ SevColApp/Controllers/GamemasterController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SevColApp.Models;
+using SevColApp.Repositories;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SevColApp.Controllers
+{
+    public class GamemasterController : Controller
+    {
+        private readonly ILogger<GamemasterController> _logger;
+        private IHomeRepository _userRepo;
+        private IBankRepository _bankRepo;
+
+        public GamemasterController(ILogger<GamemasterController> logger, IHomeRepository homeRepo, IBankRepository bankRepo)
+        {
+            _logger = logger;
+            _userRepo = homeRepo;
+            _bankRepo = bankRepo;
+        }
+
+        public IActionResult Index()
+        {
+            if (!IsThereAGameMasterCookie())
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            return View();
+        }
+
+        private bool IsThereAGameMasterCookie()
+        {
+            if (Request.Cookies.ContainsKey("UserId")) 
+            {
+                return HttpContext.Request.Cookies["UserId"] == "7777777";
+            };
+
+            return false;
+        }
+    }
+}
