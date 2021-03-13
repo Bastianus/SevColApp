bd3ecd5260477e13ef04dbc6611dc70ddf41224a
Bas Mattern <notgoingtohappen@live.nl>
2021-03-13 12:22:15 +0100

    background service opzet

---
+++ SevColApp/Hosted service/StockExchange.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using SevColApp.Context;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SevColApp.Hosted_service
+{
+    public class StockExchange : IStockExchange
+    {
+        private readonly ILogger _logger;
+        private readonly SevColContext _context;
+
+        public StockExchange(ILogger<SevColContext> logger, SevColContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task ExchangeStocks(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                _logger.LogInformation($"Exchange of stocks started at {DateTime.Now}.");
+
+                await Task.Delay(TimeSpan.FromSeconds(15), token);
+            }            
+        }
+    }
+}
---
+++ SevColApp/Hosted service/StockExchangeService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SevColApp.Hosted_service
+{
+    public class StockExchangeService : BackgroundService
+    {
+        private readonly ILogger _logger;
+
+        public IServiceProvider Services { get; }
+
+        public StockExchangeService(IServiceProvider services, ILogger<StockExchangeService> logger)
+        {
+            Services = services;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"Stock exchange service started at {DateTime.Now}.");
+
+            await DoWork(stoppingToken);
+        }
+
+        private async Task DoWork(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"Stock exchange started it's work on {DateTime.Now}.");
+
+            using (var scope = Services.CreateScope())
+            {
+                var action = scope.ServiceProvider.GetRequiredService<IStockExchange>();
+
+                await action.ExchangeStocks(stoppingToken);
+            }
+        }
+
+        public override async Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"Stock excahnge service is stopping at {DateTime.Now}.");
+
+            await base.StopAsync(stoppingToken);
+        }
+    }
+}
